9cb52c09e392aa8cce56b545c9531090ee733c14
imasahiro <imasahiro@413261d9-973d-0410-9b72-d5665a44c00b>
2009-04-07 08:39:00 +0000

    Add Makefilesvn

--- konoha-package-0.3/math/math.c
+++ konoha-package-0.3/math/math.c
@@ -1,89 +1,140 @@
-/****************************************************************************
- * KONOHA COPYRIGHT, LICENSE NOTICE, AND DISCRIMER
- *
- * Copyright (c) 2005-2009, Kimio Kuramitsu <kimio at ynu.ac.jp>
- *           (c) 2008-      Konoha Software Foundation
- * All rights reserved.
- *
- * You may choose one of the following two licenses when you use konoha.
- * See www.konohaware.org/license.html for further information.
- *
- * (1) GNU General Public License 2.0      (with    KONOHA_UNDER_GPL2)
- * (2) Konoha Software Foundation License 1.0
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
- * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- ****************************************************************************/
+#include <konoha.h>
+#include <math.h>
 
-/* ************************************************************************ */
+METHOD Math_fabs(Ctx *ctx, knh_sfp_t *sfp)
+{
+    KNH_RETURN_Float(ctx, sfp, fabs(p_double(sfp[1])));
+}
 
-#include<konoha.h>
-#define _USE_MATH_DEFINES     /* for Visual C++ */
-#include<math.h>              /* @using math -lm */
+METHOD Math_abs(Ctx *ctx, knh_sfp_t *sfp)
+{
+    KNH_RETURN_Float(ctx, sfp, fabs(p_double(sfp[1])));
+}
+METHOD Math_pow(Ctx *ctx, knh_sfp_t *sfp)
+{
+    KNH_RETURN_Float(ctx, sfp, pow(p_double(sfp[1]),p_double(sfp[2])));
+}
+METHOD Math_ldexp(Ctx *ctx, knh_sfp_t *sfp)
+{
+    KNH_RETURN_Float(ctx, sfp, ldexp(p_double(sfp[1]),p_int(sfp[2])));
+}
+METHOD Math_modf(Ctx *ctx, knh_sfp_t *sfp)
+{
+    double iprt = p_double(sfp[2]);
+    KNH_RETURN_Float(ctx, sfp, modf(p_double(sfp[1]),&iprt));
+}
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+METHOD Math_frexp(Ctx *ctx, knh_sfp_t *sfp)
+{
+    int exp = p_int(sfp[2]);
+    KNH_RETURN_Float(ctx, sfp, frexp(p_double(sfp[1]),&exp));
+}
 
-/* ------------------------------------------------------------------------ */
+METHOD Math_fmod(Ctx *ctx, knh_sfp_t *sfp)
+{
+    KNH_RETURN_Float(ctx, sfp, fmod(p_double(sfp[1]),p_double(sfp[2])));
+}
 
-static
-knh_IntConstData_t IntConstData[] = {
-	{NULL}  // end of const data
-};
+METHOD Math_ceil(Ctx *ctx, knh_sfp_t *sfp)
+{
+    KNH_RETURN_Float(ctx, sfp, ceil(p_double(sfp[1])));
+}
+
+METHOD Math_floor(Ctx *ctx, knh_sfp_t *sfp)
+{
+    KNH_RETURN_Float(ctx, sfp, floor(p_double(sfp[1])));
+}
+
+METHOD Math_sqrt(Ctx *ctx, knh_sfp_t *sfp)
+{
+    KNH_RETURN_Float(ctx,sfp,sqrt(p_double(sfp[1])));
+}
+
+METHOD Math_exp(Ctx *ctx, knh_sfp_t *sfp)
+{
+    KNH_RETURN_Float(ctx,sfp,exp(p_double(sfp[1])));
+}
+
+METHOD Math_log10(Ctx *ctx, knh_sfp_t *sfp)
+{
+    KNH_RETURN_Float(ctx,sfp,log10(p_double(sfp[1])));
+}
+
+METHOD Math_log(Ctx *ctx, knh_sfp_t *sfp)
+{
+    KNH_RETURN_Float(ctx,sfp,log(p_double(sfp[1])));
+}
+
+METHOD Math_sin(Ctx *ctx, knh_sfp_t *sfp)
+{
+    KNH_RETURN_Float(ctx, sfp, sin(p_double(sfp[1])));
+}
+
+METHOD Math_cos(Ctx *ctx, knh_sfp_t *sfp)
+{
+    KNH_RETURN_Float(ctx, sfp, cos(p_double(sfp[1])));
+}
+
+METHOD Math_tan(Ctx *ctx, knh_sfp_t *sfp)
+{
+    KNH_RETURN_Float(ctx, sfp, tan(p_double(sfp[1])));
+}
+
+METHOD Math_asin(Ctx *ctx, knh_sfp_t *sfp)
+{
+    KNH_RETURN_Float(ctx, sfp, asin(p_double(sfp[1])));
+}
+
+METHOD Math_acos(Ctx *ctx, knh_sfp_t *sfp)
+{
+    KNH_RETURN_Float(ctx, sfp, acos(p_double(sfp[1])));
+}
+
+METHOD Math_atan(Ctx *ctx, knh_sfp_t *sfp)
+{
+    KNH_RETURN_Float(ctx, sfp, atan(p_double(sfp[1])));
+}
+
+METHOD Math_atan2(Ctx *ctx, knh_sfp_t *sfp)
+{
+    KNH_RETURN_Float(ctx, sfp, atan2(p_double(sfp[1]),p_double(sfp[2])));
+}
+
+METHOD Math_sinh(Ctx *ctx, knh_sfp_t *sfp)
+{
+    KNH_RETURN_Float(ctx, sfp, sinh(p_double(sfp[1])));
+}
+
+METHOD Math_cosh(Ctx *ctx, knh_sfp_t *sfp)
+{
+    KNH_RETURN_Float(ctx, sfp, cos(p_double(sfp[1])));
+}
+
+METHOD Math_tanh(Ctx *ctx, knh_sfp_t *sfp)
+{
+    KNH_RETURN_Float(ctx, sfp, tanh(p_double(sfp[1])));
+}
 
 static
 knh_FloatConstData_t FloatConstData[] = {
-	{"math.Math.PI", M_PI},   // pi
-	{"math.Math.E", M_E},   // base of natural logarithm, e
-	{NULL}  // end of const data
+    {"math.Math.PI", M_PI},
+    {"math.Math.E", M_E},
+    {NULL}
 };
 
 static
 knh_StringConstData_t StringConstData[] = {
-	{NULL}  // end of const data
+    {NULL}
 };
-
-/* ------------------------------------------------------------------------ */
-// >>> @Singleton class Math;
-// >>> float Math.sin(float x);
-
-METHOD Math_sin(Ctx *ctx, knh_sfp_t *sfp)
-{
-	KNH_RETURN_Float(ctx, sfp, sin(p_double(sfp[1])));
-}
-
-/* ------------------------------------------------------------------------ */
-// >>> float Math.abs(float x);
-
-METHOD Math_abs(Ctx *ctx, knh_sfp_t *sfp)
-{
-	KNH_RETURN_Float(ctx, sfp, fabs(p_double(sfp[1])));
-}
-
-/* ------------------------------------------------------------------------ */
 
 KNHAPI(int) init(Ctx *ctx)
 {
-	KNH_NOTICE(ctx, "loading math ..");
-	konoha_loadIntConstData(ctx, IntConstData);
-	konoha_loadFloatConstData(ctx, FloatConstData);
-	konoha_loadStringConstData(ctx, StringConstData);
-	return 1;
+    KNH_NOTICE(ctx, "loading math...");
+    //  konoha_loadIntConstData(ctx, IntConstData);
+    konoha_loadFloatConstData(ctx, FloatConstData);
+    //konoha_loadStringConstData(ctx, StringConstData);
+    return 1;
 }
 
-/* ------------------------------------------------------------------------ */
 
-#ifdef __cplusplus
-}
-#endif
+
