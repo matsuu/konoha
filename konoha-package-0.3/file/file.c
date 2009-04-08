b3cf91e5cdf08ce6417c4a910175c3f879a50fd7
imasahiro <imasahiro@413261d9-973d-0410-9b72-d5665a44c00b>
2009-04-08 12:45:03 +0000

    create File package

---
+++ konoha-package-0.3/file/file.c
@@ -0,0 +1,66 @@
+#include <konoha.h>
+#include <string.h>
+
+void knh_file_free(Ctx *ctx,knh_Glue_t *g){
+    FILE* fp = (FILE*)g->ptr;
+    if(fp!=NULL){
+        fclose(fp);
+    }
+}
+FILE* knh_file_open(const char* fname,const char* mode){
+    FILE* fp;
+    if(mode[0] == 'w'){
+        if(mode[1] == 'b'){
+            fp = fopen(fname,"wb");
+        } else {
+            fp = fopen(fname,"w");
+        }
+        return fp;
+    } else if(mode[0] == 'r'){
+        if(mode[1] == 'b'){
+            fp = fopen(fname,"r");
+        } else {
+            fp = fopen(fname,"rb");
+        }
+        return fp;
+    } else {
+        // fprintf(stderr,"unrecognized mode. %s\n",mode);
+    }
+    return NULL;
+}
+METHOD File_new(Ctx *ctx,knh_sfp_t *sfp){
+    FILE* fp = knh_file_open(p_char(sfp[1]),p_char(sfp[2]));
+    p_cptr(sfp[0]) = fp;
+    knh_Glue_init(ctx,sfp[0].glue,fp,knh_file_free);
+    KNH_RETURN(ctx,sfp,sfp[0].o);
+}
+
+METHOD File_open(Ctx *ctx,knh_sfp_t *sfp){
+    FILE* fp = (FILE*) p_cptr(sfp[0]);
+    if(fp != NULL) {
+        knh_file_free(ctx,sfp[0].glue);
+    }
+    fp = knh_file_open(p_char(sfp[1]),p_char(sfp[1]));
+    KNH_RETURN_void(ctx,sfp);
+}
+
+METHOD File_putc(Ctx *ctx,knh_sfp_t *sfp){
+    FILE* fp = (FILE*) p_cptr(sfp[0]);
+    int i = (int)p_int(sfp[1]);
+    if(fp == NULL){
+        KNH_THROWs(ctx,"file is not open.");
+    }
+    fputc(i,fp);
+    KNH_RETURN_void(ctx,sfp);
+}
+
+METHOD File_close(Ctx *ctx,knh_sfp_t *sfp){
+    FILE* fp = (FILE*) p_cptr(sfp[0]);
+    if(fp == NULL){
+        KNH_WARNING(ctx, "file is already closed.");
+    } else{
+        fclose(fp);
+        p_cptr(sfp[0]) = NULL;
+    }
+    KNH_RETURN_void(ctx,sfp);
+}
