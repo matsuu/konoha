ba7e928c090555867d6d4b5aaa8adbfa92f32e89
imasahiro <imasahiro@413261d9-973d-0410-9b72-d5665a44c00b>
2009-06-04 06:26:48 +0000

    fix bug: knh_Stmt_checkOPSIZE

--- src/compiler/typing.c
+++ src/compiler/typing.c
@@ -2146,7 +2146,7 @@ knh_class_t knh_StmtOPEQ_basecid(Ctx *ctx, Stmt *stmt)
 static
 int knh_Stmt_checkOPSIZE(Ctx *ctx,Stmt *stmt, size_t n)
 {
-	if(DP(stmt)->size + 1 == n) return 1;
+	if(DP(stmt)->size == n + 1) return 1;
 	if(n == 1) {
 		knh_perror(ctx, SP(stmt)->fileid, SP(stmt)->line, KERR_ERROR, _("must be uniary operator"));
 	}
