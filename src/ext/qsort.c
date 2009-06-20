3459955a4c4f4a6aa4fa334ac39fc7568199005a
imasahiro <imasahiro@413261d9-973d-0410-9b72-d5665a44c00b>
2009-06-20 11:30:31 +0000

    bug fixed: update qsort

--- src/ext/qsort.c
+++ src/ext/qsort.c
@@ -1,39 +1,73 @@
 #ifdef KONOHA_OS__LKM
-#include <linux/slab.h>
 #include <linux/string.h>
+#define _quicksort qsort
+#else
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #endif
 
-#define SWAP(s, i, j, n)\
-  {						\
-    memcpy(n,i,s);				\
-    memcpy(i,j,s);				\
-    memcpy(j,n,s);				\
-  }
+typedef char qstack_t;
+#define STACK_SIZE      (128 * sizeof(size_t))
+#define STACK_PUSH(stack,ptr) ((void) ((stack = (ptr)), ++stack))
+#define STACK_POP( stack,ptr) ((void) (--stack, ptr= stack))
+#define STACK_IS_EMPTY(stack,ptr) (stack >= ptr)
 
-void qsort(void *a, size_t elt, size_t s, int (*compare)(const void *,const void *))
+/* Byte-wise swap two items of size SIZE. */
+#define SWAP(a, b, size) {                  \
+    register size_t __size = (size);        \
+    register char *__a = (a), *__b = (b);   \
+    do {                                    \
+        char __tmp = *__a;                  \
+        *__a++ = *__b;                      \
+        *__b++ = __tmp;                     \
+    } while (--__size > 0);                 \
+}                                           \
+
+void _quicksort (void *const base, size_t total_elems, size_t size,
+        int (*cmp)(const void*,const void*))
 {
-    if(s > 0 && elt > 1){
-        void *i, *j, *v, *n;
-        n = kmalloc(s,GFP_KERNEL);
-        v = a + elt * s - 1 * s;
-        i = a;
-        j = a + elt * s - 2 * s;
-        while(1){
-            while(compare(i,v) < 0){
-                i += s;
-            };
-            while(compare(j,v) >= 0){
-                j -= s;
-            };
-            if(i >= j){
+    char *base_ptr = (char *) base;
+    if (total_elems == 0) return;
+    char *left  = base_ptr;
+    char *right = &left[size * (total_elems - 1)];
+    qstack_t stack[STACK_SIZE];
+    qstack_t *top = stack;
+
+    STACK_PUSH (top, NULL);
+
+    while (!STACK_IS_EMPTY(stack,top))
+    {
+        char *left_ptr  = left;
+        char *right_ptr = right;
+        char *mid = left + size * ((right - left) / size >> 1);
+
+        while (left_ptr <= right_ptr) {
+            while ((*cmp) ((void *) left_ptr, (void *) mid)  < 0) left_ptr  += size;
+            while ((*cmp) ((void *) mid, (void *) right_ptr) < 0) right_ptr -= size;
+
+            if (left_ptr < right_ptr) {
+                SWAP (left_ptr, right_ptr, size);
+                if (mid == left_ptr)        mid = right_ptr;
+                else if (mid == right_ptr)  mid = left_ptr;
+                left_ptr  += size;
+                right_ptr -= size;
+            }
+            else if (left_ptr == right_ptr) {
+                left_ptr  += size;
+                right_ptr -= size;
                 break;
             }
-            SWAP(s,i,j,n);
         }
-        SWAP(s,i,v,n);
-        kfree(n);
-        qsort(a,(i-a)/s,s,compare);
-        qsort(i+s,elt-(i-a+s)/s,s,compare);
+        if((right_ptr - left) > (right - left_ptr)){
+            STACK_PUSH(top,left);
+            STACK_PUSH(top,right_ptr);
+            left = left_ptr;
+        } else {
+            STACK_POP(top,left_ptr);
+            STACK_POP(top,right);
+            right = right_ptr;
+        }
     }
 }
 
