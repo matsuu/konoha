9482f2a706c165fa46c83eeaa3e433165bcaf3e0
goccy <goccy@413261d9-973d-0410-9b72-d5665a44c00b>
2009-07-01 21:41:13 +0000

    Add files for teaboard compile.

---
+++ tbmx1/support/include/inttypes.h
@@ -0,0 +1,6 @@
+#ifndef _DEFINE_H_INTTYPES_
+#define _DEFINE_H_INTTYPES_
+
+#include <stdint.h>
+
+#endif /* _DEFINE_H_INTTYPES_ */
---
+++ tbmx1/support/src/getenv.c
@@ -0,0 +1,8 @@
+#include <basic.h>
+
+char* ALT_getenv(const char* name)
+{
+  char* ret = "";
+    return ret;
+}
+
