9097c6d624a583e831c59ad860b4b3488299ae68
imasahiro <imasahiro@413261d9-973d-0410-9b72-d5665a44c00b>
2009-07-03 11:08:25 +0000

    update packages:surpport 64bit linux

---
+++ package-experimental/curl/linux_64.mk
@@ -0,0 +1,27 @@
+# modified by u.hira@users.sourceforge.jp
+
+CC = gcc
+CFLAGS = -O2 -Wall -fmessage-length=0 -fPIC
+
+LDLIBS = -lcurl -lkonoha
+
+target = linux_64
+pkgname = curl
+
+library = "$(pkgname)_$(target).so"
+
+.PHONY: all
+all: $(library)
+
+objs = "$(pkgname).o"
+
+"$(pkgname).o": $(pkgname).c
+	$(CC) $(CFLAGS) -o $@ -c $^
+
+$(library): $(objs)
+	$(CC) -shared -Wl -o $@ $^ $(LDLIBS)
+
+.PHONY: clean
+clean:
+	$(RM) -rf $(objs) $(library)
+
