0739d9bafa079931f571f595cfddc6b6939511b7
yashiromann <yashiromann@413261d9-973d-0410-9b72-d5665a44c00b>
2009-07-06 00:40:51 +0000

    Added files for T-Shell build directory

---
+++ tshell/support/include/machine.h
@@ -0,0 +1,8 @@
+/*
+ * This file is used for including the original machine.h,
+ * but for undefining "Asm" macro
+ */
+
+#include_next <machine.h>
+#undef Asm
+
---
+++ tshell/support/src/getenv.c
@@ -0,0 +1,7 @@
+#include <basic.h>
+
+char* ALT_getenv(const char* name)
+{
+    return NULL;
+}
+
